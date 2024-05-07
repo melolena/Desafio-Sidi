a8bb250260bb65c53ded97f52b1461c280c168b5
melolena <milenasantosmelo@gmail.com>
2024-05-07 20:42:54 -0300

    iniciando o header

--- verdinhos/src/pages/home/home.tsx
+++ verdinhos/src/pages/home/home.tsx
@@ -1,5 +1,9 @@
 import logoSidi from '../../assets/logo-sidi-nome.png';
-
+import Avatar from '@mui/material/Avatar';
+import Mauricio from '../../assets/mauricioG.jpg';
+import Sino  from '../../assets/maskNotification.svg';
+import Suporte from '../../assets/maskSuport.svg';
+import Sair from '../../assets/maskLogout.svg'
 function Home() {
     return (
       <div className="home">
@@ -8,9 +12,23 @@ function Home() {
           <div className="menu">
           <Avatar
             alt="Remy Sharp"
-            src="/static/images/avatar/1.jpg"
+            src={Mauricio}
             sx={{ width: 56, height: 56 }}
           />
+          <div id="identificador">
+              <h3>Maurício Gabriel</h3>
+              <div id="iconsMenu">
+                <a className="IconMenu">
+                  <img src={Sino}/>
+                </a>
+                <a className="IconMenu">
+                  <img src={Suporte}/>
+                </a>
+                <a className="IconMenu">
+                  <img src={Sair}/>
+                </a>
+              </div>
+          </div>
           </div>
         </div>
       </div>
