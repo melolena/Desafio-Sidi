9d239d989dfeb14b6fb0788612898d3ef9877668
melolena <milenasantosmelo@gmail.com>
2024-05-15 18:52:26 -0300

    adicionar Pausa

--- verdinhos/src/pages/baterPonto/baterPonto.tsx
+++ verdinhos/src/pages/baterPonto/baterPonto.tsx
@@ -10,13 +10,21 @@ import { GiExitDoor } from "react-icons/gi";
 import { FaCoffee } from "react-icons/fa";
 import { MdHistory } from "react-icons/md";
 import dayjs from 'dayjs';
+import RelogioPausa from '../../components/RelogioPausa';
 
 function BaterPonto() {
   const [showBaterPonto, setShowBaterPonto] = useState(false);
   const [showEntradaBox, setShowEntradaBox] = useState(false);
-  const [showConfirmar, setShowConfirmar] = useState(false);
+  const [showConfirmarEntrada, setShowConfirmarEntrada] = useState(false);
+  const [showConfirmarSaida, setShowConfirmarSaida] = useState(false);
+  const [showSaidaBox, setShowSaidaBox] = useState(false);
+  const [showPausaBox, setShowPausaBox] = useState(false);
+  const [showConfirmarPausa, setShowConfirmarPausa] = useState(false);
   const [selectedDay, setSelectedDay] = useState(dayjs());
-  const [currentTime, setCurrentTime] = useState(dayjs());
+  const [currentTime, setCurrentTime] = useState(dayjs());  
+  const [pausaEndTime, setPausaEndTime] = useState(dayjs());
+  const [mensagemPausa, setMensagemPausa] = useState('');
+
   useEffect(() => {
     setShowBaterPonto(true);
   }, []);
@@ -25,7 +33,10 @@ function BaterPonto() {
     setSelectedDay(day);
     setShowBaterPonto(true);
     setShowEntradaBox(false);
-    setShowConfirmar(false); 
+    setShowConfirmarEntrada(false); 
+    setShowSaidaBox(false);
+    setShowConfirmarSaida(false);
+    setShowPausaBox(false);
   };
 
   const handlePonto = (tipo) => {
@@ -34,18 +45,91 @@ function BaterPonto() {
       setCurrentTime(dayjs());
       setShowEntradaBox(true);
       setShowBaterPonto(false);
+      setShowConfirmarEntrada(false);
+      setShowSaidaBox(false);
+      setShowConfirmarSaida(false);
+      setShowPausaBox(false);
+    }
+
+    if (tipo === "Saida") {
+      setCurrentTime(dayjs());
+      setShowSaidaBox(true);
+      setShowConfirmarEntrada(false);
+      setShowBaterPonto(false);
+      setShowEntradaBox(false);
+      setShowConfirmarSaida(false);
+      setShowPausaBox(false);
+    }
+
+    if (tipo === "Pausa") {
+      setCurrentTime(dayjs());
+      setShowPausaBox(true);
+      setShowConfirmarEntrada(false);
+      setShowBaterPonto(false);
+      setShowEntradaBox(false);
+      setShowSaidaBox(false);
+      setShowConfirmarSaida(false);
+      setShowSaidaBox(false);
     }
   };
 
   const handleVoltar = () => {
     setShowEntradaBox(false);
     setShowBaterPonto(true);
+    setShowSaidaBox(false);
+    setShowConfirmarSaida(false);
+    setShowEntradaBox(false);
+    setShowConfirmarEntrada(false);
+    setShowPausaBox(false);
   };
 
-  const handleConfirmar = () =>{
-    setShowConfirmar(true);
+  const handleConfirmarEntrada = () =>{
+    setShowConfirmarEntrada(true);
     setShowEntradaBox(false);
-  }
+    setShowSaidaBox(false);
+    setShowBaterPonto(false);
+    setShowSaidaBox(false);
+    setShowConfirmarSaida(false);
+    setShowPausaBox(false);
+  };
+
+  const handleConfirmarSaida = () =>{
+    setShowConfirmarSaida(true);
+    setShowConfirmarEntrada(false);
+    setShowSaidaBox(false);
+    setShowBaterPonto(false);
+    setShowEntradaBox(false);
+    setShowPausaBox(false);
+  };
+
+  const handlePausa = () =>{
+    setShowPausaBox(true);
+    setShowConfirmarEntrada(false);
+    setShowBaterPonto(false);
+    setShowEntradaBox(false);
+    setShowSaidaBox(false);
+    setShowConfirmarSaida(false);
+    setShowSaidaBox(false);
+  };
+
+  const handleConfirmarPausaFinal = () =>{
+    setShowConfirmarPausa(true);
+    setShowPausaBox(false);
+    setShowConfirmarEntrada(false);
+    setShowBaterPonto(false);
+    setShowEntradaBox(false);
+    setShowSaidaBox(false);
+    setShowConfirmarSaida(false);
+    setShowSaidaBox(false);
+  };
+
+  const handleConfirmarPausa = (endTime) => {
+    setPausaEndTime(endTime);
+    setShowPausaBox(false);
+    const intervaloMinutos = endTime.diff(currentTime, 'minute');
+    const mensagemPausa = `Tudo pronto! Sua pausa da data ${selectedDay.format('DD/MM/YYYY')}, no horário ${currentTime.format('HH:mm')} e com intervalo de ${intervaloMinutos} minutos foi realizada com sucesso!`;
+    setMensagemPausa(mensagemPausa);
+  };
 
   return (
     <div className="baterPonto">
@@ -95,7 +179,7 @@ function BaterPonto() {
               <h3>{currentTime.format('HH:mm')}</h3>
             </div>
             <div className="BotoesComponentesMenu"> 
-              <Button onClick={handleConfirmar}>
+              <Button onClick={handleConfirmarEntrada}>
                 Confirmar
               </Button>
               <Button onClick={handleVoltar}>
@@ -104,7 +188,7 @@ function BaterPonto() {
             </div>
           </div>
         ) }
-        {showConfirmar &&(
+        {showConfirmarEntrada &&(
           <div className="validationBox">
             <h2>Confirmar Entrada</h2>
             <h3 id="subtituloConfirmacao">Entrada Confirmada!</h3>
@@ -112,6 +196,62 @@ function BaterPonto() {
             <p id="rodape">Confira as notificações para mais informações, e qualquer dúvida ou problema consulte o suporte.</p>
           </div>
         )}
+        {showSaidaBox &&(
+          <div className="validationBox">
+          <h2>Saída</h2>
+          <div className="dataRegistada">
+            <h3>Data</h3>
+            <h3>{selectedDay.format('DD/MM/YYYY')}</h3>
+          </div>
+          <div className="horaRegistrada">
+            <h3>Horário</h3>
+            <h3>{currentTime.format('HH:mm')}</h3>
+          </div>
+          <div className="BotoesComponentesMenu"> 
+            <Button onClick={handleConfirmarSaida}>
+              Confirmar
+            </Button>
+            <Button onClick={handleVoltar}>
+              Voltar
+            </Button>
+          </div>
+        </div>
+        )}
+        {showConfirmarSaida &&(
+          <div className="validationBox">
+            <h2>Confirmar Saída</h2>
+            <h3 id="subtituloConfirmacao">Saída Confirmada!</h3>
+            <p>Tudo pronto! Sua saída da data {selectedDay.format('DD/MM/YYYY')} e no horário {currentTime.format('HH:mm')} foi relizada com sucesso!</p>
+            <p id="rodape">Confira as notificações para mais informações, e qualquer dúvida ou problema consulte o suporte.</p>
+          </div>
+        )}
+        {showPausaBox &&(
+          <div className="validationBox" id="pausaBox">
+          <h2>Pausa</h2>
+          <div className="dataRegistada">
+            <h3>Data</h3>
+            <h3>{selectedDay.format('DD/MM/YYYY')}</h3>
+          </div>
+          <div className="horaRegistrada">
+            <h3>Horário</h3>
+            <h3>{currentTime.format('HH:mm')}</h3>
+          </div>
+          <div id="horarioFinal">
+            <p>Defina o horário de término do intevalo</p>
+            <RelogioPausa/>
+          </div>
+          <div className="BotoesComponentesMenu"> 
+            <Button onClick={handleConfirmarPausaFinal}>
+              Confirmar
+            </Button>
+            <Button onClick={handleVoltar}>
+              Voltar
+            </Button>
+          </div>
+        </div>
+        )}
+        
+
       </div>
     </div>
   );
