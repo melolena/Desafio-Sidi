9740392366eeb1fa1e58cda2b49f08b15f92536d
melolena <milenasantosmelo@gmail.com>
2024-06-12 16:44:31 -0300

    corrigindo erros

--- verdinhos/src/components/Calendario.tsx
+++ verdinhos/src/components/Calendario.tsx
@@ -1,81 +1,53 @@
 import React from 'react';
-import Badge from '@mui/material/Badge';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar, PickersDay, pickersDayClasses, PickersDayProps } from '@mui/x-date-pickers';
+import { DateCalendar, PickersDay, PickersDayProps } from '@mui/x-date-pickers';
 import { Dayjs } from 'dayjs';
-import 'dayjs/locale/pt-br';
-import '../pages/baterPonto/styleBP.css';
-
-interface ServerDayProps extends PickersDayProps<Dayjs> {
-  onDayClick: (day: Dayjs) => void;
-}
-
-const ServerDay: React.FC<ServerDayProps> = (props) => {
-  const { day, outsideCurrentMonth, onDayClick, ...other } = props;
-
-  return (
-    <Badge key={day.toString()} overlap="circular" badgeContent={undefined}>
-      <PickersDay
-        {...other}
-        outsideCurrentMonth={outsideCurrentMonth}
-        day={day}
-        onClick={() => onDayClick(day)}
-        sx={{
-          backgroundColor: 'white !important',
-          color: 'black !important',
-          fontFamily: 'Josefin Sans !important',
-          fontWeight: '500 !important',
-          fontSize: '15px !important',
-          [`&.${pickersDayClasses.selected}`]: {
-            backgroundColor: '#3FD48F !important',
-            color: 'white !important',
-            '&:hover, &:focus': {
-              backgroundColor: '#3FD48F !important',
-              color: 'white !important',
-            },
-          },
-          [`&:hover`]: {
-            backgroundColor: '#3FD48F !important',
-            color: 'white !important',
-          },
-        }}
-      />
-    </Badge>
-  );
-};
 
 interface CalendarioProps {
-  onDayClick: (day: Dayjs) => void;
-  selectedDay: Dayjs;
+  value: Dayjs;
+  onChange: (newValue: Dayjs | null) => void;
 }
 
-const Calendario: React.FC<CalendarioProps> = ({ onDayClick, selectedDay }) => {
-  return (
-    <LocalizationProvider
-      dateAdapter={AdapterDayjs}
-      adapterLocale="pt-br"
-    >
-      <DateCalendar
-        value={selectedDay}
-        onChange={(newDay) => {
-          if (newDay) {
-            onDayClick(newDay);
-          }
-        }}
-        renderDay={(day, _selectedDays, pickersDayProps) => (
-          <ServerDay {...pickersDayProps} day={day} onDayClick={onDayClick} />
-        )}
+const Calendario: React.FC<CalendarioProps> = ({ value, onChange }) => {
+  const renderDay = (
+    day: Dayjs,
+    _selectedDays: Array<Dayjs | null>,
+    pickersDayProps: PickersDayProps<Dayjs>
+  ) => {
+    return (
+      <PickersDay
+        {...pickersDayProps}
+        day={day}
         sx={{
-          backgroundColor: '#471D7C !important',
-          borderRadius: '20px !important',
-          '.Mui-selected': {
+          '&:hover, &:focus': {
+            backgroundColor: '#471D7C !important',
+            color: 'white !important',
+          },
+          ...(pickersDayProps.outsideCurrentMonth && {
+            opacity: 0.5,
+          }),
+        }}
+      />
+    );
+  };
+
+  return (
+    <DateCalendar<Dayjs>
+      value={value}
+      onChange={onChange}
+      renderDay={renderDay}
+      sx={{
+        backgroundColor: '#471D7C !important',
+        borderRadius: '16px',
+        '.Mui-selected': {
+          backgroundColor: '#3FD48F !important',
+          color: 'white !important',
+          '&:hover, &:focus': {
             backgroundColor: '#3FD48F !important',
             color: 'white !important',
           },
-        }}
-      />
-    </LocalizationProvider>
+        },
+      }}
+    />
   );
 };
 
